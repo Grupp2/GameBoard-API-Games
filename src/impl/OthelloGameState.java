bc18d8d5b22e4c07fd3521e0bd0793db084daca2
evr- <erik_266@hotmail.com>
2014-04-16 14:47:23 +0200

    GameState added

--- src/impl/OthelloGameState.java
+++ src/impl/OthelloGameState.java
@@ -1,13 +1,19 @@
 package impl;
 
 import game.api.GameState;
-import game.impl.Board;
-import game.impl.Move;
-import game.impl.Player;
+import game.impl.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OthelloGameState implements GameState {
+
+    private List<Player> players;
+    private Board gameBoard;
+
+    public OthelloGameState(){
+        reset();
+    }
     @Override
     public List<Player> getPlayers() {
         return null;
@@ -50,6 +56,34 @@ public class OthelloGameState implements GameState {
 
     @Override
     public void reset() {
+        players = new ArrayList<Player>();
 
+        gameBoard = new Board(makeBoardLocations());
+
+
+        players.add(new Player("P1", makePlayerOneGamePieces()));
+        players.add(new Player("P2", makePlayerTwoGamePieces()));
+
+    }
+
+    private List<GamePiece> makePlayerOneGamePieces(){
+        return null;
+    }
+
+    private List<GamePiece> makePlayerTwoGamePieces(){
+        return null;
+    }
+
+    private List<BoardLocation> makeBoardLocations(){
+
+        List<BoardLocation> list = new ArrayList<BoardLocation>();
+
+        for(int i = 0; i < 8; i++){
+            for(int j = (int)'A'; i < (int)'I'; j++){
+                list.add(new BoardLocation(""+(char)j+i));
+            }
+        }
+
+        return list;
     }
 }
