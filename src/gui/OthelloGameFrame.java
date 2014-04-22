03a9a6b943bb5eec172dabeafbb4997e17637f9c
ntn11phm <ntn11phm@student.hig.se>
2014-04-22 14:47:55 +0200

    GameFrame updated

--- src/gui/OthelloGameFrame.java
+++ src/gui/OthelloGameFrame.java
@@ -13,16 +13,25 @@ public class OthelloGameFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private Player player = new Player("P1", Arrays.asList(new GamePiece("X"), new GamePiece("X"), new GamePiece("X")));
 	private JPanel contentPane;
+	private JPanel p1Panel;
+	private JPanel p2Panel;
 	
 	public OthelloGameFrame() {
-		setBounds(100, 100, 825, 600);
+		setBounds(1, 1, 800, 600);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new JPanel();
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		contentPane.add(new PlayerPanel(player, true, 150, 150));
-		
+		p1Panel = new PlayerPanel(player, true, 150, 150);
+		p2Panel = new PlayerPanel(player, true, 150, 150);
+		contentPane.add(p1Panel);
+		setCtrlBounds();
+	}
+	
+	private void setCtrlBounds(){
+		p1Panel.setBounds(10, 10, 150, 150 * 32);
+		p2Panel.setBounds(640, 10, 150, 150 * 32);
 	}
 
 }
