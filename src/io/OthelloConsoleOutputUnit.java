4e582c22c627cdfb96e16344fed0b325416a4669
Johan Hugg <karljohanhugg@gmail.com>
2014-04-22 20:18:19 +0200

    Added method getOutputString
    When the publish method is called it will print out the string returned
    from getOutputString. This is so the console output can be tested in a
    JUnit test by calling the new method.

--- src/io/OthelloConsoleOutputUnit.java
+++ src/io/OthelloConsoleOutputUnit.java
@@ -1,35 +1,36 @@
 package io;
 
+
 import game.api.GameState;
 import game.impl.Board;
-import game.impl.BoardLocation;
 import game.io.OutputUnit;
 import game.impl.*;
 
-/**
- * Created by HuggTop on 2014-04-16.
- */
 public class OthelloConsoleOutputUnit implements OutputUnit {
     @Override
     public void publish(GameState gameState) {
-        Board board = gameState.getBoard();
+        System.out.println(getOutputString(gameState));    
+    }
+    
+    String getOutputString(GameState gameState){
+    	Board board = gameState.getBoard();
         GamePiece piece;
-        System.out.println("  1  2  3  4  5  6  7  8");
+        String outputString = new String();
+        outputString += "  1  2  3  4  5  6  7  8 \n";
         char[] rows = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
         int b = 0;
         int lim = 8;
         for (int i = 0; i < 8; i++) {
-        	System.out.print(rows[i] + " ");
+        	outputString += (rows[i] + " ");
         	for (;b < lim; b++) {
         		if ((piece = board.getLocations().get(b).getPiece()) != null)
-        			System.out.print(piece.getId() + "  ");
+        			outputString += piece.getId() + "  ";
         		else
-        			System.out.print("  ");
+        			outputString += "  ";
         	}
         	lim += 8;
-        	System.out.println();
+        	outputString += "\n";
         }
-            
-
+        return outputString;
     }
 }
