98dda8c42a92f33d5311b08f0f44ea4264a14630
ntn11phm <ntn11phm@student.hig.se>
2014-04-24 09:58:14 +0200

    Cleanup
    Råkade trycka ctrl+shift+f

--- testsrc/io/OthelloConsoleOutputUnitTest.java
+++ testsrc/io/OthelloConsoleOutputUnitTest.java
@@ -6,20 +6,22 @@ import impl.OthelloGameState;
 import io.OthelloConsoleOutputUnit;
 import junit.framework.TestCase;
 import static org.mockito.Mockito.*;
+
 /**
  * Created by HuggTop on 2014-04-16.
  */
 public class OthelloConsoleOutputUnitTest extends TestCase {
-    private OthelloConsoleOutputUnit consoleOutputUnit;
+	private OthelloConsoleOutputUnit consoleOutputUnit;
 	private OthelloGameState gameState;
-    public void setUp() throws Exception {
-        super.setUp();
+
+	public void setUp() throws Exception {
+		super.setUp();
 		gameState = new OthelloGameState();
 		gameState.reset();
-        consoleOutputUnit = new OthelloConsoleOutputUnit();
-    }
+		consoleOutputUnit = new OthelloConsoleOutputUnit();
+	}
 
-    public void testPublish() {
-consoleOutputUnit.publish(gameState);
-    }
+	public void testPublish() {
+		consoleOutputUnit.publish(gameState);
+	}
 }
