9148dc7060f5dd2bbf3d56879584f8fb1dbd01d8
hugg <hugg@Johan-ThinkPad>
2014-04-24 09:32:58 +0200

    testResetPublish() test written for console output

--- testsrc/io/OthelloConsoleOutputUnitTest.java
+++ testsrc/io/OthelloConsoleOutputUnitTest.java
@@ -1,25 +1,44 @@
 package io;
 
-import game.api.GameState;
-import game.io.OutputUnit;
 import impl.OthelloGameState;
 import io.OthelloConsoleOutputUnit;
 import junit.framework.TestCase;
-import static org.mockito.Mockito.*;
 /**
  * Created by HuggTop on 2014-04-16.
  */
 public class OthelloConsoleOutputUnitTest extends TestCase {
     private OthelloConsoleOutputUnit consoleOutputUnit;
 	private OthelloGameState gameState;
+	private char p = '·';
+	
     public void setUp() throws Exception {
         super.setUp();
 		gameState = new OthelloGameState();
-		gameState.reset();
         consoleOutputUnit = new OthelloConsoleOutputUnit();
     }
 
-    public void testPublish() {
-consoleOutputUnit.publish(gameState);
+    public void testResetPublish() {
+    	gameState.reset();
+    	String[] outputString = consoleOutputUnit.getOutputString(gameState).split("\\n");
+    	if (!outputString[0].equals("  1  2  3  4  5  6  7  8 "))
+    		fail("Line 1 is incorrect!");
+    	if (!outputString[1].equals("A ·  ·  ·  ·  ·  ·  ·  ·  "))
+    		fail("Line 2 is incorrect!");
+    	if (!outputString[2].equals("B ·  ·  ·  ·  ·  ·  ·  ·  "))
+    		fail("Line 3 is incorrect!");
+    	if (!outputString[3].equals("C ·  ·  ·  ·  ·  ·  ·  ·  "))
+    		fail("Line 4 is incorrect!");
+    	if (!outputString[4].equals("D ·  ·  ·  O  X  ·  ·  ·  "))
+    		fail("Line 5 is incorrect!");
+    	if (!outputString[5].equals("E ·  ·  ·  X  O  ·  ·  ·  "))
+    		fail("Line 6 is incorrect!");
+    	if (!outputString[6].equals("F ·  ·  ·  ·  ·  ·  ·  ·  "))
+    		fail("Line 7 is incorrect!");
+    	if (!outputString[7].equals("G ·  ·  ·  ·  ·  ·  ·  ·  "))
+    		fail("Line 8 is incorrect!");
+    	if (!outputString[8].equals("H ·  ·  ·  ·  ·  ·  ·  ·  "))
+    		fail("Line 9 is incorrect!");
+    	
+    	
     }
 }
