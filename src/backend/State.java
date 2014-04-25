c8f7944f482f29a3c82f85f610569e1811c81b0d
evr- <erik_266@hotmail.com>
2014-04-25 22:52:50 +0200

    Undo/redo support in State

--- src/backend/State.java
+++ src/backend/State.java
@@ -2,6 +2,7 @@ package backend;
 
 import java.util.List;
 
+import backend.undoableactions.UndoableAction;
 import game.impl.Board;
 import game.impl.Player;
 
@@ -16,6 +17,10 @@ public class State {
     private Player lastPlayer;
 
     private Player currentPlayer;
+
+    private List<UndoableAction> actions;
+
+    private int currentActionIndex;
 
 
     public List<Player> getPlayers() {
@@ -62,4 +67,45 @@ public class State {
         this.currentPlayer = player;
     }
 
+
+    public void setActions(List<UndoableAction> actions){
+        this.actions = actions;
+    }
+
+    public List<UndoableAction> getActions(){
+        return actions;
+    }
+
+
+    public void setCurrentActionIndex(int currentActionIndex){
+        this.currentActionIndex = currentActionIndex;
+    }
+
+    public int getCurrentActionIndex(){
+        return currentActionIndex;
+    }
+
+
+    public void pushAction(UndoableAction action){
+        for(int i = actions.size(); i > currentActionIndex; i--){
+            actions.remove(i);
+        }
+
+        actions.add(action);
+        currentActionIndex++;
+    }
+
+    public void undo(){
+        if(currentActionIndex < 0)
+            throw new RuntimeException("No more actions to undo!");
+
+        actions.get(currentActionIndex--).undo();
+    }
+
+    public void redo(){
+        if(currentActionIndex >= actions.size())
+            throw new RuntimeException("No more actions to redo!");
+
+        actions.get(currentActionIndex++).execute();
+    }
 }
