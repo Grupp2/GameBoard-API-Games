a51b8b17c7462628b92f869cce6a8056dbd2a540
ntn11phm <ntn11phm@student.hig.se>
2014-04-29 15:22:15 +0200

    removed PlayerPanels from GUI

--- src/gui/OthelloGameFrame.java
+++ src/gui/OthelloGameFrame.java
@@ -2,29 +2,22 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
-
 import game.api.GameState;
 import game.io.OutputUnit;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-
 import backend.OthelloGameState;
 
 public class OthelloGameFrame extends JFrame {
-
 	private static final long serialVersionUID = 1L;
 	private GameState gameState;
 	private JPanel contentPane;
-	private JPanel p1Panel;
-	private JPanel p2Panel;
 	private JPanel gameBoardPanel;
 	private JLabel lblStatusText;
-	private JButton btnPassTurn;
 	private GameBoardListener gameBoardListener;
 	private GraphicsHolder gh = new GraphicsHolder();
 	private OthelloGuiInputUnit inputUnit;
@@ -32,7 +25,7 @@ public class OthelloGameFrame extends JFrame {
 	
 	public OthelloGameFrame(OthelloGuiInputUnit inputUnit) {
 		this.inputUnit = inputUnit;
-		setBounds(1, 1, 675, 600);
+		setBounds(1, 1, 605, 600);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
@@ -46,20 +39,15 @@ public class OthelloGameFrame extends JFrame {
 	}
 	
 	public void buildGameFrame() {
-		createPlayerPanels();
 		createGameBoardPanel();
 		gameBoardListener = new GameBoardListener(gameBoardPanel, inputUnit);
 		contentPane = new JPanel(new BorderLayout());
 		setContentPane(contentPane);
-		lblStatusText = new JLabel("Game started!");
+		lblStatusText = new JLabel(gameState.getPlayerInTurn().getName());
 		lblStatusText.setHorizontalAlignment(SwingConstants.CENTER);
 		lblStatusText.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnPassTurn = new JButton("Pass turn!");
 		contentPane.add(lblStatusText, BorderLayout.PAGE_START);
-		contentPane.add(p1Panel, BorderLayout.LINE_START);
 		contentPane.add(gameBoardPanel, BorderLayout.CENTER);
-		contentPane.add(p2Panel, BorderLayout.LINE_END);
-		contentPane.add(btnPassTurn, BorderLayout.PAGE_END);
 		this.setVisible(true);
 	}
 	
@@ -70,12 +58,7 @@ public class OthelloGameFrame extends JFrame {
 	public void setGameBoardPanel(JPanel gameBoardPanel) {
 		this.gameBoardPanel = gameBoardPanel;
 	}
-	
-	private void createPlayerPanels() {
-		final int CONTROL_SIZE = 100;
-		p1Panel = new PlayerPanelSimple(gameState.getPlayers().get(0), CONTROL_SIZE, CONTROL_SIZE);
-		p2Panel = new PlayerPanelSimple(gameState.getPlayers().get(1), CONTROL_SIZE, CONTROL_SIZE);
-	}
+
 	
 	private void createGameBoardPanel() {
 		gameBoardPanel = new GameBoardPanel(gameState);
