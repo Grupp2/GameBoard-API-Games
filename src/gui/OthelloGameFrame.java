5c792e4a5c0e5ccab723a69549281003a726216b
ntn11phm <ntn11phm@student.hig.se>
2014-04-29 13:06:47 +0200

    GUI GameFrame placeGamePieces fixed missing getId()

--- src/gui/OthelloGameFrame.java
+++ src/gui/OthelloGameFrame.java
@@ -91,10 +91,10 @@ public class OthelloGameFrame extends JFrame implements OutputUnit {
 	
 	private void placeGamePieces() {
 		for (int i = 0; i < gameState.getBoard().getLocations().size();i++) {
-			if (gameState.getBoard().getLocations().get(i).getPiece()!=null) {
-				if (gameState.getBoard().getLocations().get(i).getPiece().equals("X"))
+			if (gameState.getBoard().getLocations().get(i).getPiece() != null) {
+				if (gameState.getBoard().getLocations().get(i).getPiece().getId().equals("X"))
 					((JButton)gameBoardPanel.getComponent(i)).setIcon(new ImageIcon(gh.getPlayer1Piece()));
-				else if (gameState.getBoard().getLocations().get(i).getPiece().equals("O"))
+				else if (gameState.getBoard().getLocations().get(i).getPiece().getId().equals("O"))
 					((JButton)gameBoardPanel.getComponent(i)).setIcon(new ImageIcon(gh.getPlayer2Piece()));
 			} else
 				((JButton)gameBoardPanel.getComponent(i)).setIcon(new ImageIcon(gh.getBlankBoardPiece()));
