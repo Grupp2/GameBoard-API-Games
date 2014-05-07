4c84ce6be9de957f7e53079adf6d14842371ba28
ntn11phm <ntn11phm@student.hig.se>
2014-05-07 18:25:37 +0200

    refactoring gui.Listeners

---
+++ src/gui/Listeners/GameBoardListener.java
@@ -0,0 +1,38 @@
+package gui.Listeners;
+
+import gui.OthelloGuiInputUnit;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class GameBoardListener {
+	private JButton currentButton;
+	private OthelloGuiInputUnit inputUnit;
+	private JPanel gameBoard;
+
+	public GameBoardListener(JPanel gameBoard, OthelloGuiInputUnit inputUnit) {
+		this.inputUnit = inputUnit;
+		this.gameBoard = gameBoard;
+	}
+	
+	public void addButtonListeners() {
+		for (Component ctrl : gameBoard.getComponents())
+			if (ctrl instanceof JButton) {
+				JButton btn = (JButton) ctrl;
+				btn.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						currentButton = (JButton) e.getSource();
+						createMove();
+					}
+				});
+			}
+	}
+	
+	private void createMove() {
+		inputUnit.notifyListeners(currentButton.getName());
+	}
+}
---
+++ src/gui/Listeners/UtilityListener.java
@@ -0,0 +1,51 @@
+package gui.Listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import game.api.GameState;
+import gui.OthelloGuiInputUnit;
+import gui.OthelloUtillityPanel;
+import backend.OthelloGameState;
+
+public class UtilityListener {
+	private OthelloUtillityPanel utilityPanel;
+	private OthelloGameState gameState;
+	private OthelloGuiInputUnit inputUnit;
+
+	public UtilityListener(OthelloUtillityPanel utilityPanel,GameState gameState, OthelloGuiInputUnit inputUnit) {
+		this.utilityPanel = utilityPanel;
+		this.inputUnit = inputUnit;
+		this.gameState = (OthelloGameState) gameState;
+	}
+
+	public void createButtonListeners() {
+		utilityPanel.getBtnExit().addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {exitGame();}});
+		utilityPanel.getBtnNew().addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {newGame();}});
+		utilityPanel.getBtnLoad().addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {loadGame();}});
+		utilityPanel.getBtnSave().addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {saveGame();}});
+		utilityPanel.getBtnUndo().addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {undoMove();}});
+	}
+	private void exitGame(){
+		System.exit(0);
+	}
+	private void newGame(){
+		gameState.reset();
+		inputUnit.notifyListeners("republish");
+	}
+	private void loadGame(){
+		
+	}
+	private void saveGame(){
+		
+	}
+	
+	private void undoMove(){
+		if(gameState.canUndo()){
+			gameState.undo();
+			inputUnit.notifyListeners("republish");
+		}
+						
+	}
+
+}
