bcd0d06a692b10b88896a9176e9a1fb196abddce
ntn11phm <ntn11phm@student.hig.se>
2014-05-07 13:01:29 +0200

    changed to GridBagLayout for statusPanel

--- src/gui/OthelloStatusPanel.java
+++ src/gui/OthelloStatusPanel.java
@@ -1,6 +1,8 @@
 package gui;
 
-import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -9,13 +11,31 @@ public class OthelloStatusPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JLabel lblStatusText;
 	private JLabel lblPlayerInfo;
+	private GridBagConstraints c = new GridBagConstraints();
+	private Color backgroundGreen = new Color(34, 177, 76, 255);
 	
 	public OthelloStatusPanel() {
-		setLayout(new GridLayout(1, 2));
+		setLayout(new GridBagLayout());
+		setBackground(backgroundGreen);
 		this.lblPlayerInfo = new JLabel();
 		this.lblStatusText = new JLabel();
-		add(lblPlayerInfo);
-		add(lblStatusText);
+		addPlayerInfoLabel();
+		addStatusTextLabel();
+	}
+
+	private void addPlayerInfoLabel() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		add(lblPlayerInfo, c);
+	}
+	
+	private void addStatusTextLabel() {
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridwidth = 2;
+		c.gridx = 1;
+		c.gridy = 0;
+		add(lblStatusText, c);
 	}
 	
 	public OthelloStatusPanel getStatusPanel() {
