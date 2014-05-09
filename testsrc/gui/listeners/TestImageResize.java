970f1cc5f2cebeaa7de04ead30c4ddaa75105e88
ntn11phm <ntn11phm@student.hig.se>
2014-05-09 14:51:23 +0200

    added TestImageResize

---
+++ testsrc/gui/listeners/TestImageResize.java
@@ -0,0 +1,42 @@
+package gui.listeners;
+
+import static org.junit.Assert.*;
+import gui.graphics.ImageResize;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestImageResize {
+	private BufferedImage testImage;
+	private Dimension dimensionToTest;
+	private ImageResize ir = new ImageResize();
+	
+	@Before
+	public void setUp() throws Exception {
+		try {
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			InputStream input = classLoader.getResourceAsStream("gui/graphics/svart.jpg");
+			testImage = ImageIO.read(input);
+		} catch (IOException e) {
+			
+		}
+	}
+
+	@Test
+	public void HorisontalTest() {
+		dimensionToTest = new Dimension(20, 40);
+		testImage = ir.resizeImage(testImage, dimensionToTest);
+		assertEquals(20, testImage.getWidth());
+	}
+	
+	@Test
+	public void VerticalTest() {
+		dimensionToTest = new Dimension(40, 20);
+		testImage = ir.resizeImage(testImage, dimensionToTest);
+		assertEquals(20, testImage.getHeight());
+	}
+}
