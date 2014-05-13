e06cf9897556af16adc821b47d62439f4e183078
Johan Hugg <hugg@Johan-ThinkPad.(none)>
2014-05-13 13:15:01 +0200

    Modified Main class to start GUI by default or use console with a command line argument.

--- src/run/Program.java
+++ src/run/Program.java
@@ -3,12 +3,25 @@ package run;
 import backend.OthelloGameState;
 import io.OthelloConsoleIOFactory;
 import game.init.Runner;
+import gui.OthelloGameFrame;
+import gui.OthelloGuiIOFactory;
+import gui.OthelloGuiInputUnit;
+import gui.OthelloGuiOutputUnit;
 
 public class Program
  {
 
 	public static void main(String[] args) {
-		new Runner(new OthelloGameState(), new OthelloConsoleIOFactory()).run();
+	    	if (args.length > 0) {
+	    	    for (int i = 0; i < args.length; i++) {
+	    		if (args[i].equals("-console"))
+	    		    new Runner(new OthelloGameState(), new OthelloConsoleIOFactory()).run();
+	    	    }
+	    	} else {
+    	    	    OthelloGuiInputUnit inputUnit = new OthelloGuiInputUnit();
+    		    OthelloGuiOutputUnit outputUnit = new OthelloGuiOutputUnit(new OthelloGameFrame(inputUnit));
+    		    new Runner(new OthelloGameState(), new OthelloGuiIOFactory(inputUnit, outputUnit)).run();
+	    	}
 	}
 
 }
