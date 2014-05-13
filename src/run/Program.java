1af8316c96100e8f5485ece5f0c6af704910bf4a
ntn11phm <ntn11phm@student.hig.se>
2014-05-13 16:47:39 +0200

    added -c switch and refactoring

--- src/run/Program.java
+++ src/run/Program.java
@@ -8,20 +8,17 @@ import gui.OthelloGuiIOFactory;
 import gui.OthelloGuiInputUnit;
 import gui.OthelloGuiOutputUnit;
 
-public class Program
- {
+public class Program {
 
 	public static void main(String[] args) {
-	    	if (args.length > 0) {
-	    	    for (int i = 0; i < args.length; i++) {
-	    		if (args[i].equals("-console"))
-	    		    new Runner(new OthelloGameState(), new OthelloConsoleIOFactory()).run();
-	    	    }
-	    	} else {
-    	    	    OthelloGuiInputUnit inputUnit = new OthelloGuiInputUnit();
-    		    OthelloGuiOutputUnit outputUnit = new OthelloGuiOutputUnit(new OthelloGameFrame(inputUnit));
-    		    new Runner(new OthelloGameState(), new OthelloGuiIOFactory(inputUnit, outputUnit)).run();
-	    	}
+		if (args.length > 0) {
+			for (int i = 0; i < args.length; i++)
+				if (args[i].equals("-console") || args[i].equals("-c"))
+					new Runner(new OthelloGameState(), new OthelloConsoleIOFactory()).run();
+		} else {
+			OthelloGuiInputUnit inputUnit = new OthelloGuiInputUnit();
+			OthelloGuiOutputUnit outputUnit = new OthelloGuiOutputUnit(new OthelloGameFrame(inputUnit));
+			new Runner(new OthelloGameState(), new OthelloGuiIOFactory(inputUnit, outputUnit)).run();
+		}
 	}
-
 }
