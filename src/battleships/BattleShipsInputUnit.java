cc7302b6cd6bb99757db91fa8e5d5cb74b7b6994
Johan Hugg <hugg@Johan-ThinkPad.(none)>
2014-05-15 13:02:11 +0200

    Added BattleShipsInputUnit

---
+++ src/battleships/BattleShipsInputUnit.java
@@ -0,0 +1,18 @@
+package battleships;
+
+import game.api.GameState;
+import game.io.InputUnit;
+
+public class BattleShipsInputUnit extends InputUnit{
+
+    @Override
+    public void setup(GameState state) {
+	
+	
+    }
+    
+    public void notifyListeners(String loc) {
+	
+    }
+
+}
