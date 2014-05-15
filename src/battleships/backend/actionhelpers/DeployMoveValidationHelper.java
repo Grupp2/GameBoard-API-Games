a9f4072251888271b777df91c484269279622cad
ntn11phm <ntn11phm@student.hig.se>
2014-05-15 22:49:18 +0200

    added MoveValidation logic

--- src/battleships/backend/actionhelpers/DeployMoveValidationHelper.java
+++ src/battleships/backend/actionhelpers/DeployMoveValidationHelper.java
@@ -1,8 +1,21 @@
 package battleships.backend.actionhelpers;
 
+import battleships.backend.State;
+import battleships.backend.classhelpers.DeployMoveHelper;
 import game.impl.Move;
 
 public class DeployMoveValidationHelper implements MoveValidatable {
+	private State state;
+    private DeployMoveHelper moveHelper;
+	
+	public DeployMoveValidationHelper(State state, DeployMoveHelper moveHelper){
+        this.state = state;
+        this.moveHelper = moveHelper;
+    }
+	
+	public DeployMoveValidationHelper(State state){
+        this(state, new DeployMoveHelper(state));
+    }
 
 	@Override
 	public boolean makeMoveValidation(Move move) {
---
+++ src/battleships/backend/classhelpers/DeployMoveHelper.java
@@ -0,0 +1,12 @@
+package battleships.backend.classhelpers;
+
+import battleships.backend.State;
+
+public class DeployMoveHelper {
+	private State state;
+	
+	public DeployMoveHelper(State state){
+		this.state = state;
+	}
+
+}
