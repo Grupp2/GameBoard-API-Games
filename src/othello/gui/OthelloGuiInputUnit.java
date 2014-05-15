fb76e8d0c457b5c4b96e8da8f85cec83a71a28ad
Johan Hugg <hugg@Johan-ThinkPad.(none)>
2014-05-15 10:26:23 +0200

    Formatting fix

--- src/othello/gui/OthelloGuiInputUnit.java
+++ src/othello/gui/OthelloGuiInputUnit.java
@@ -7,51 +7,51 @@ import game.io.InputUnit;
 import java.util.List;
 
 public class OthelloGuiInputUnit extends InputUnit {
-	private GameState state;
-	private OthelloGameFrame gameFrame;
-	private String input;
-	
-	private Move getNextMove(GameState state) {
-        if(input.toLowerCase().equals("republish"))
-            return null;
+    private GameState state;
+    private OthelloGameFrame gameFrame;
+    private String input;
 
-		Move result = null;
-		try {
+    private Move getNextMove(GameState state) {
+	if (input.toLowerCase().equals("republish"))
+	    return null;
 
-			result = new Move(state.getPlayerInTurn(), new GamePiece(
-					getGamePieceID(state.getPlayerInTurn(), state)),
-					getLocationById(state.getBoard(), input));
-		} catch (Exception ex) {
-			gameFrame.setStatusLabelText(ex.getMessage());
-		}
-		return result;
+	Move result = null;
+	try {
+
+	    result = new Move(state.getPlayerInTurn(), new GamePiece(
+		    getGamePieceID(state.getPlayerInTurn(), state)),
+		    getLocationById(state.getBoard(), input));
+	} catch (Exception ex) {
+	    gameFrame.setStatusLabelText(ex.getMessage());
 	}
-
-	private String getGamePieceID(Player player, GameState gameState) {
-		if (player == gameState.getPlayers().get(0))
-			return "O";
-		else
-			return "X";
-	}
-
-    private BoardLocation getLocationById(Board board, String id){
-        List<BoardLocation> locations = board.getLocations();
-
-        for(int i = 0; i < locations.size(); i++)
-            if(locations.get(i).getId().equals(id))
-                return locations.get(i);
-
-        return null;
+	return result;
     }
 
-	@Override
-	public void setup(GameState state) {
-		this.state = state;
-		this.state.reset();
-	}
+    private String getGamePieceID(Player player, GameState gameState) {
+	if (player == gameState.getPlayers().get(0))
+	    return "O";
+	else
+	    return "X";
+    }
 
-	public void notifyListeners(String inputTileName) {
-		input = inputTileName;
-		notifyListenersOfMove(getNextMove(state));
-	}
+    private BoardLocation getLocationById(Board board, String id) {
+	List<BoardLocation> locations = board.getLocations();
+
+	for (int i = 0; i < locations.size(); i++)
+	    if (locations.get(i).getId().equals(id))
+		return locations.get(i);
+
+	return null;
+    }
+
+    @Override
+    public void setup(GameState state) {
+	this.state = state;
+	this.state.reset();
+    }
+
+    public void notifyListeners(String inputTileName) {
+	input = inputTileName;
+	notifyListenersOfMove(getNextMove(state));
+    }
 }
