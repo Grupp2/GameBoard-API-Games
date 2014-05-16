c9e53b5aa23e78c4852f660da2aa6d29894cee4e
Johan Hugg <karljohanhugg@gmail.com>
2014-05-16 10:05:01 +0200

    Added turncounter

---
+++ src/battleships/backend/actionhelpers/TurnCounter.java
@@ -0,0 +1,25 @@
+package battleships.backend.actionhelpers;
+
+import battleships.backend.State;
+
+public class TurnCounter {
+    private int deployMovesRemaining = 40;
+    private State state;
+    
+    public TurnCounter(State state) {
+	this.state = state;
+    }
+    
+    public int getRemainingDeployMoves() {
+	return deployMovesRemaining;
+    }
+    
+    public int decrementMoveCounter() {
+	deployMovesRemaining--;
+	
+	if (deployMovesRemaining == 0)
+	    state.setIsDeployMode(false);
+	return deployMovesRemaining;
+    }
+    
+}
