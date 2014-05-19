66590f2bc9f434d01ee391aab4ec04d756ed50c3
ntn11phm <ntn11phm@student.hig.se>
2014-05-19 15:25:28 +0200

    added DeployBoardHelperTest

---
+++ testsrc/battleships/backend/classhelpers/DeployBoardHelperTest.java
@@ -0,0 +1,19 @@
+package battleships.backend.classhelpers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DeployBoardHelperTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
