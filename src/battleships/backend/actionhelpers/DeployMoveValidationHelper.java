db7830fe99066814a9b15a3353ba08418f85ba8e
ntn11phm <ntn11phm@student.hig.se>
2014-05-19 23:17:48 +0200

    updated DeployMoveValidationHelper

--- src/battleships/backend/actionhelpers/DeployMoveValidationHelper.java
+++ src/battleships/backend/actionhelpers/DeployMoveValidationHelper.java
@@ -37,32 +37,14 @@ public class DeployMoveValidationHelper implements MoveValidatable {
             state.setMessage("");
             return false;
         }
-
-        if(!isPlayersTurnToMove(move)){
-            state.setMessage("It's not your turn!");
-            return false;
-        }
-
-//        if(!doesDestinationExist(move)){
-//            state.setMessage("Invalid location!");
-//            return false;
-//        }
-
-        if(!isDestinationEmpty(move)){
-            state.setMessage("There is already a piece in that location!");
-            return false;
-        }
-
-        if(isTryingToMovePieceAlreadyPlaced(move)){
-            state.setMessage("You cannot move a piece already placed on the board.");
-            return false;
-        }
-
-//        if(!isValidOthelloMove(move)){
-//            state.setMessage("You have to put your piece at a valid location!");
-//            return false;
-//        }
-
+		state.setMessage(validatePiece(move));
+		if (!state.getMessage().equals(""))
+			return false;
+		
+		state.setMessage(validateBoard());
+		if (!state.getMessage().equals(""))
+			return false;
+		
         state.setMessage("");
         return true;
 	}
@@ -70,24 +52,4 @@ public class DeployMoveValidationHelper implements MoveValidatable {
 	public boolean isRequestForRepublish(Move move){
         return move == null;
     }
-
-    public boolean isPlayersTurnToMove(Move move){
-        return move.getPlayer() == state.getCurrentPlayer();
-    }
-
-//    public boolean doesDestinationExist(Move move){
-//        return move.getDestination() != null && boardHelper.doesLocationExistOnBoard(move.getDestination());
-//    }
-
-    public boolean isDestinationEmpty(Move move){
-        return move.getDestination().getPiece() == null;
-    }
-
-    public boolean isTryingToMovePieceAlreadyPlaced(Move move){
-        return move.getSource() != null;
-    }
-
-//    public boolean isValidOthelloMove(Move move){
-//        return moveHelper.isLocationValidForDeployForPlayer(move.getDestination(), move.getPlayer());
-//    }
 }
