2cd52630cb967283bd3bf4971ff4bae38fd410ab
ntn11med <ntn11med@student.hig.se>
2014-05-20 09:59:16 +0200

    Getters added

--- src/battleships/gui/panels/BattleshipGamePanels.java
+++ src/battleships/gui/panels/BattleshipGamePanels.java
@@ -9,29 +9,39 @@ import game.api.GameState;
 import gui.GameBoardSizeCalculator;
 
 public class BattleshipGamePanels {
-	private GameState gameState;
 	private JPanel player1;
 	private JPanel player2;
-	
-	public BattleshipGamePanels(GameState gameState, TranslatorAdapter ta){
-		
+
+	public BattleshipGamePanels(GameState gameState, TranslatorAdapter ta) {
+		separationOfPlayers(gameState, ta);
+
 	}
-	private void separationOfPlayers(GameState gameState,TranslatorAdapter ta){
+
+	private void separationOfPlayers(GameState gameState, TranslatorAdapter ta) {
 		player1 = new JPanel();
 		player2 = new JPanel();
 		GameBoardSizeCalculator gc = new GameBoardSizeCalculator();
 		int[] xy = gc.calculateBoardSize(gameState, ta);
-		player1.setLayout(new GridLayout(xy[1]/2, xy[0]));
-		player2.setLayout(new GridLayout(xy[1]/2, xy[0]));
-		BattleshipGameBoardPanel gb = new BattleshipGameBoardPanel(gameState, ta);
-		
-		for(int i = 0 ; i < 100 ; i++){
+		player1.setLayout(new GridLayout(xy[1] / 2, xy[0]));
+		player2.setLayout(new GridLayout(xy[1] / 2, xy[0]));
+		BattleshipGameBoardPanel gb = new BattleshipGameBoardPanel(gameState,
+				ta);
+
+		for (int i = 0; i < 100; i++) {
 			player1.add(gb.getComponent(i));
 		}
-		for(int i = 100; i < 200; i++){
+		for (int i = 100; i < 200; i++) {
 			player2.add(gb.getComponent(i));
 		}
-		
+
+	}
+
+	public JPanel getPlayer1() {
+		return player1;
+	}
+
+	public JPanel getPlayer2() {
+		return player2;
 	}
 
 }
