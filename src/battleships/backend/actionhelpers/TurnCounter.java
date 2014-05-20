6a80b2da8ebf4079b4aab2e32898677249feea4a
Johan Hugg <karljohanhugg@gmail.com>
2014-05-20 13:40:13 +0200

    deployMovesRemaining from 40 to 20.

--- src/battleships/backend/actionhelpers/TurnCounter.java
+++ src/battleships/backend/actionhelpers/TurnCounter.java
@@ -3,23 +3,23 @@ package battleships.backend.actionhelpers;
 import battleships.backend.State;
 
 public class TurnCounter {
-    private int deployMovesRemaining = 40;
+    private int deployMovesRemaining = 20;
     private State state;
     
     public TurnCounter(State state) {
-	this.state = state;
+	    this.state = state;
     }
     
     public int getRemainingDeployMoves() {
-	return deployMovesRemaining;
+	    return deployMovesRemaining;
     }
     
     public int decrementMoveCounter() {
-	deployMovesRemaining--;
-	
-	if (deployMovesRemaining == 0)
-	    state.setIsDeployMode(false);
-	return deployMovesRemaining;
+        deployMovesRemaining--;
+
+        if (deployMovesRemaining == 0)
+            state.setIsDeployMode(false);
+        return deployMovesRemaining;
     }
     
 }
