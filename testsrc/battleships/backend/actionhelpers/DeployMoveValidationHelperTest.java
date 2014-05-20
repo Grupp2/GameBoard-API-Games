7cbb2ae118ba52cc74952b54038ea4be2af73c8f
ntn11phm <ntn11phm@student.hig.se>
2014-05-20 13:08:57 +0200

    added DeployMoveValidationHelperTest

---
+++ testsrc/battleships/backend/actionhelpers/DeployMoveValidationHelperTest.java
@@ -0,0 +1,30 @@
+package battleships.backend.actionhelpers;
+
+import static org.junit.Assert.*;
+import game.impl.GamePiece;
+import game.impl.Move;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import battleships.backend.State;
+
+public class DeployMoveValidationHelperTest {
+	private DeployMoveValidationHelper dmvh;
+	private State state;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.state = new State();
+		ResetHelper rs = new ResetHelper(state);
+		rs.reset();
+		this.dmvh = new DeployMoveValidationHelper(state);
+	}
+
+	@Test
+	public void test() {
+		dmvh.makeMoveValidation(new Move(state.getCurrentPlayer(), new GamePiece(id), destination));
+		fail("Not yet implemented");
+	}
+
+}
