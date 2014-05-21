24962f0f3fbfbe7a154ee18e303962001ce387ef
ntn11phm <ntn11phm@student.hig.se>
2014-05-21 20:00:39 +0200

    added tests to MoveToPieceConverterTest

--- testsrc/battleships/backend/classhelpers/MoveToPieceConverterTest.java
+++ testsrc/battleships/backend/classhelpers/MoveToPieceConverterTest.java
@@ -15,6 +15,11 @@ public class MoveToPieceConverterTest {
 	}
 
 	@Test
+	public void pieceLocationsColumnReverseTest() {
+		List<BoardLocation> result = mpc.pieceLocations(new BoardLocation("A5"), new BoardLocation("A1"));
+		assertEquals("A2", result.get(1).getId());
+	}
+	@Test
 	public void pieceLocationsColumnTest() {
 		List<BoardLocation> result = mpc.pieceLocations(new BoardLocation("A1"), new BoardLocation("A5"));
 		assertEquals("A2", result.get(1).getId());
@@ -26,4 +31,9 @@ public class MoveToPieceConverterTest {
 		assertEquals("B1", result.get(1).getId());
 	}
 
+	@Test
+	public void pieceLocationsRowReverseTest() {
+		List<BoardLocation> result = mpc.pieceLocations(new BoardLocation("D1"), new BoardLocation("A1"));
+		assertEquals("B1", result.get(1).getId());
+	}
 }
