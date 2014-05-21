a431dccf036f4eacc12cf9e85653e955e735e0b2
ntn11phm <ntn11phm@student.hig.se>
2014-05-21 09:17:35 +0200

    updated setup() in BattleShipsInputUnit

--- src/battleships/gui/BattleShipsInputUnit.java
+++ src/battleships/gui/BattleShipsInputUnit.java
@@ -4,11 +4,12 @@ import game.api.GameState;
 import game.io.InputUnit;
 
 public class BattleShipsInputUnit extends InputUnit{
-
+	private GameState state;
+	
     @Override
     public void setup(GameState state) {
-	
-	
+    	this.state = state;
+        this.state.reset();
     }
     
     public void notifyListeners(String loc) {
