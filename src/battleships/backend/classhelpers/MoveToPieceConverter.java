fd6f65474b303e6a955cc9f4844f5ee498f87928
ntn11phm <ntn11phm@student.hig.se>
2014-05-21 15:50:56 +0200

    fixed critical bug

--- src/battleships/backend/classhelpers/MoveToPieceConverter.java
+++ src/battleships/backend/classhelpers/MoveToPieceConverter.java
@@ -13,41 +13,39 @@ public class MoveToPieceConverter {
 		Integer firstCoordinateNumber = Integer.parseInt(firstCoordinate.getId().substring(1));
 		Integer secondCoordinateNumber = Integer.parseInt(secondCoordinate.getId().substring(1));
 		int size;
-		boolean isIncreasing = false;
+
 		if (firstCoordinateLetterAsInteger == secondCoordinateLetterAsInteger)
 			size = Math.abs(firstCoordinateNumber - secondCoordinateNumber) + 1;
 		else
 			size = Math.abs(firstCoordinateLetterAsInteger - secondCoordinateLetterAsInteger) + 1;
 		if (firstCoordinateLetterAsInteger == secondCoordinateLetterAsInteger) {
-			if (firstCoordinateNumber < secondCoordinateNumber)
-				isIncreasing = true;
+			if (firstCoordinateNumber > secondCoordinateNumber){
+				BoardLocation tmp = firstCoordinate;
+				firstCoordinate = secondCoordinate;
+				secondCoordinate = tmp;
+			}
 		} else 
-			if (firstCoordinateLetterAsInteger < secondCoordinateLetterAsInteger)
-				isIncreasing = true;
+			if (firstCoordinateLetterAsInteger > secondCoordinateLetterAsInteger){
+				BoardLocation tmp = firstCoordinate;
+				firstCoordinate = secondCoordinate;
+				secondCoordinate = tmp;
+			}
+		firstCoordinateLetterAsInteger = (int)firstCoordinate.getId().charAt(0);
+		secondCoordinateLetterAsInteger = (int)secondCoordinate.getId().charAt(0);
+		firstCoordinateNumber = Integer.parseInt(firstCoordinate.getId().substring(1));
+		secondCoordinateNumber = Integer.parseInt(secondCoordinate.getId().substring(1));
 		List<BoardLocation> result = new ArrayList<BoardLocation>(size);
 		for (int i = 0; i < size; i++)
 			if (firstCoordinateLetterAsInteger == secondCoordinateLetterAsInteger) {
-				if (isIncreasing){
 					String letterValue = ""+(char)(firstCoordinateLetterAsInteger);
-					firstCoordinateNumber = firstCoordinateNumber+i;
 					String numberValue = firstCoordinateNumber.toString();
 					result.add(new BoardLocation(letterValue+numberValue));
-				} else {
-					String letterValue = ""+(char)(firstCoordinateLetterAsInteger);
-					secondCoordinateNumber = secondCoordinateNumber+i;
-					String numberValue = secondCoordinateNumber.toString();
-					result.add(new BoardLocation(letterValue+numberValue));
-				}
-			} else
-				if (isIncreasing){
+					firstCoordinateNumber = firstCoordinateNumber+1;
+			} else {
 					String letterValue = ""+(char)(firstCoordinateLetterAsInteger+i);
 					String numberValue = firstCoordinateNumber.toString();
 					result.add(new BoardLocation(letterValue+numberValue));
-				} else {
-					String letterValue = ""+(char)(secondCoordinateLetterAsInteger+i);
-					String numberValue = secondCoordinateNumber.toString();
-					result.add(new BoardLocation(letterValue+numberValue));
-				}
+			}
 		return result;
 	}
 }
