b3537fde1da63d690a2744a5e821d58d0441920b
ntn11phm <ntn11phm@student.hig.se>
2014-05-21 12:20:40 +0200

    trying to sortout deployPanel

--- src/client/games/BattleShipsGame.java
+++ src/client/games/BattleShipsGame.java
@@ -1,6 +1,9 @@
 package client.games;
 
+import java.awt.Color;
+
 import battleships.backend.BattleShipsGameState;
+import battleships.backend.Settings;
 import battleships.gui.BattleShipsInputUnit;
 import battleships.gui.ContentPanel;
 import battleships.gui.GUIUpdater;
@@ -8,13 +11,17 @@ import battleships.gui.panels.BattleshipDeployPanel;
 import game.api.GameState;
 import game.init.Runner;
 import game.io.OutputUnit;
+import generics.GameBoardPanel;
 import generics.GameIoFactory;
 import generics.GameOutputUnit;
 
 import javax.swing.*;
 
-public class BattleShipsGame implements GameStartup{
+import translator.CoordinateTranslator;
+import translator.TranslatorAdapter;
 
+public class BattleShipsGame implements GameStartup{
+	private final Color BACKGROUND_COLOR = new Color(130, 170, 130, 255);
     private GameState gameState;
     private GameIoFactory ioFactory;
     private JPanel contentPanel;
@@ -28,7 +35,8 @@ public class BattleShipsGame implements GameStartup{
         gameState.reset();
 
         BattleShipsInputUnit inputUnit = new BattleShipsInputUnit();
-
+        TranslatorAdapter ta = new TranslatorAdapter(new CoordinateTranslator());
+        GameBoardPanel gamePanels = new GameBoardPanel(gameState, ta, BACKGROUND_COLOR);
         BattleshipDeployPanel deployPanel = new BattleshipDeployPanel(gameState, inputUnit);
 
         ContentPanel contentPanel = new ContentPanel(inputUnit, deployPanel);
@@ -41,6 +49,16 @@ public class BattleShipsGame implements GameStartup{
         this.ioFactory = new GameIoFactory(inputUnit, outputUnit);
         this.contentPanel = contentPanel;
     }
+    
+    private JPanel getCurrentPlayerBoard(GameBoardPanel gamePanels, BattleshipDeployPanel deployPanel) {
+		if (gameState.getPlayerInTurn().getName().equals(Settings.PLAYER_ONE_NAME))
+			deployPanel = gamePanels.getPlayer2();
+		else
+			deployPanel = gamePanels.getPlayer1();
+		deployListener.createButtonListeners();
+		return deployPanel;
+	}
+
 
     @Override
     public JPanel getContentPane() {
