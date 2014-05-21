97d46c7ecd9370b767ec5f9424ab747efa212bc5
Johan Hugg <karljohanhugg@gmail.com>
2014-05-21 09:03:18 +0200

    NormalMoveExecutor implemented.

--- src/battleships/backend/actionhelpers/NormalMoveExecutor.java
+++ src/battleships/backend/actionhelpers/NormalMoveExecutor.java
@@ -1,7 +1,12 @@
 package battleships.backend.actionhelpers;
 
+import battleships.backend.Settings;
 import battleships.backend.State;
+import game.impl.BoardLocation;
+import game.impl.GamePiece;
 import game.impl.Move;
+
+import java.util.List;
 
 /**
  * Created by hugg on 2014-05-21.
@@ -16,6 +21,17 @@ public class NormalMoveExecutor implements MoveExecutable {
     public boolean executeMove(Move move, Move firstMove) {
         //firstmove is only for deploymode
         //we only care about move here.
+        List<BoardLocation> boardLocations = state.getBoard().getLocations();
+        BoardLocation locationToAlter = boardLocations.get(boardLocations.indexOf(move.getDestination()));
+
+        switch (state.getMessage()) {
+            case Settings.PIECE_SHIPHIT_MESSAGE:
+                locationToAlter.setPiece(new GamePiece(Character.toString(Settings.PIECE_ALREADYHIT)));
+                return true;
+            case Settings.PIECE_MISS_MESSAGE:
+                locationToAlter.setPiece(new GamePiece(Character.toString(Settings.PIECE_ALREADYHIT)));
+                return true;
+        }
 
         return false;
     }
